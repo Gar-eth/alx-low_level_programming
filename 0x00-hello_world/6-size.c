4ed47519759d774ca4a015153f45153cd8955d22
Gar-eth <tompoigareth@gmail.com>
2023-03-16 16:46:29 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point.prints the size of various types on the computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of int: %d byte(s)\n", sizeof(int));
 printf("Size of short: %d byte(s)\n", sizeof(short));
