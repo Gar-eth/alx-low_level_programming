6e549c9110ef8a6809084ff13cb3aa0a2cd733f8
Gar-eth <tompoigareth@gmail.com>
2023-03-17 14:18:46 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,35 +1,24 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
- * main - Prints a text according numbeer
- * Return: Always (Success)
+ * main - get random int and compare last digit to 0, 5 and 6
  *
+ * Return: 0 always
  */
-
-int main (void)
-
+int main(void)
 {
+	int n;
 
-	int n, lastd;
-
-	srand(time)(0));
-	n = rand() - RAND_MAX / 2 ;
-	lastd = n % 10;
-
-	if (lastd > 5)
-        }
-        printf("Last digit of %d is %d and is greater that 5\n", n, lastd);
-	}
-	else if (lastd == 0)
-	}
-	printf("last digit of %d is %d and is 0\n", n,lastd);
-	}
-        else if (lastd < 6 && lastd!=0)
-        {
-        printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-	}
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d and", n, n % 10);
+	if (n % 10  == 0)
+		printf(" is 0\n");
+	else if (n % 10 > 5)
+		printf(" is greater than 5\n");
+	else
+		printf(" is less than 6 and not 0\n");
+	return (0);
+}
